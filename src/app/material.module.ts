f6c5ab27f5da3ccb084adae90ab4df231273b7bf
cgreen <cgreen@atiba.com>
2019-12-10 14:22:43 -0600

    Added sample components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,18 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
